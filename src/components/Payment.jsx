b73b2a477ac74d216296d3d59508a2084b3de26e
mtsarah <sarah_berger@outlook.com>
2023-03-25 00:51:24 +0100

    add routing

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -9,7 +9,8 @@ export default function Payment() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setOpen(true)}>
+        onClick={() => setOpen(true)}
+      >
         Open Modal
       </button>
       <Modal open={open} setOpen={setOpen}>
