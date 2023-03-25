1b2c5f3397c2673d6123cfffde8292cbe1a32391
mtsarah <sarah_berger@outlook.com>
2023-03-25 15:36:05 +0100

    modal

--- src/components/AcceptModal.jsx
+++ src/components/AcceptModal.jsx
@@ -4,53 +4,75 @@ import Button from "./Button";
 import AskForGeolocation from "./AskForGeolocation";
 import { CheckIcon, MapPinIcon } from "@heroicons/react/24/outline";
 import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function AcceptModal({ open, setOpen, requestData }) {
   const [confirmation, setConfirmation] = useState(false);
   const { user, cash, distance } = requestData || {};
   return (
-    <>
-      <Modal
-        open={open}
-        setOpen={setOpen}
-        button={<Button onClick={() => setConfirmation(true)}>Accept</Button>}
-      >
-        <p>
-          Will you help out {user} with {cash}?
-        </p>
-        <p>They are {distance} away:</p>
-        <div className="m-2">
-          <Map
-            longitude={16.37351128650816}
-            latitude={48.20904017128513}
-            zoom={15}
-            mapboxAccessToken="pk.eyJ1IjoiZmxvYmF1IiwiYSI6ImNrdW1uYW12cDFlenUzM282Ym96N3pqYTEifQ.RH29qvuc6pkcbl5JxtDzVQ"
-            style={{ width: "100%", height: 200 }}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
+    <Modal
+      open={open}
+      setOpen={setOpen}
+      button={
+        !confirmation ? (
+          <Button onClick={() => setConfirmation(true)}>Accept</Button>
+        ) : (
+          <Button
+            onClick={() => {
+              setConfirmation(false);
+            }}
           >
-            <Marker
-              key={user}
-              longitude={16.374028026924933}
-              latitude={48.18843507581358}
+            Close
+          </Button>
+        )
+      }
+      className="max-w-7xl"
+    >
+      {!confirmation ? (
+        <>
+          <p>
+            Will you help out {user?.substring(0, 7)} with {cash}?
+          </p>
+          <p>They are {distance} away:</p>
+          <div className="m-2">
+            <Map
+              longitude={16.37351128650816}
+              latitude={48.20904017128513}
+              zoom={15}
+              mapboxAccessToken="pk.eyJ1IjoiZmxvYmF1IiwiYSI6ImNrdW1uYW12cDFlenUzM282Ym96N3pqYTEifQ.RH29qvuc6pkcbl5JxtDzVQ"
+              style={{ width: "100%", height: 200 }}
+              mapStyle="mapbox://styles/mapbox/streets-v9"
             >
-              <MapPinIcon className="w-5 h-5 inline" />
-            </Marker>
-          </Map>
-        </div>
-      </Modal>
-      <Modal
-        open={confirmation}
-        setOpen={setConfirmation}
-        button={<Button onClick={() => setConfirmation(false)}>Close</Button>}
-      >
+              <Marker
+                key={user}
+                longitude={16.37291046977881}
+                latitude={48.20968720834627}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="black"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Marker>
+            </Map>
+          </div>
+        </>
+      ) : (
         <div className="flex flex-col gap-6">
-          <p>Thanks mate! Get on your way to help out {user}</p>
+          <p>Thanks mate! go catch em all!</p>
           <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
             <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
           </div>
         </div>
-      </Modal>
-    </>
+      )}
+    </Modal>
   );
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,15 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { CheckIcon } from "@heroicons/react/24/outline";
+import classNames from "classnames";
 
-export default function Modal({ title, children, button, open, setOpen }) {
+export default function Modal({
+  title,
+  children,
+  button,
+  open,
+  setOpen,
+  className,
+}) {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={setOpen}>
@@ -13,7 +20,8 @@ export default function Modal({ title, children, button, open, setOpen }) {
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0">
+          leaveTo="opacity-0"
+        >
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
@@ -26,13 +34,20 @@ export default function Modal({ title, children, button, open, setOpen }) {
               enterTo="opacity-100 translate-y-0 sm:scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel
+                className={classNames(
+                  "relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6",
+                  className ? className : ""
+                )}
+              >
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
                       as="h3"
-                      className="text-base font-semibold leading-6 text-gray-900">
+                      className="text-base font-semibold leading-6 text-gray-900"
+                    >
                       {title}
                     </Dialog.Title>
                     <div className="mt-2">{children}</div>
