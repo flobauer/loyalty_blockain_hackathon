b6c5d511874008cd55e35bfde514328aa782a4b4
Florian Bauer <florian@sbg.at>
2023-03-25 02:16:28 +0100

    Working!!!!!

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,82 +3,12 @@ import { MapPinIcon, PlusIcon } from "@heroicons/react/24/outline";
 import Button from "./Button";
 import PaymentModal from "./PaymentModal";
 
-export default function GlobalFeed() {
-  const now = useRef(new Date()); // Make sure current time isn't re-rendered
-
-  const tempData = [
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "slskjslkjlkjkldjf",
-      cash: "50€",
-      distance: "20km",
-    },
-    {
-      user: "skljksdfjlsldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skleeeedjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skleeeedjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-    {
-      user: "skldjf",
-      cash: "40€",
-      distance: "10km",
-    },
-  ];
-
+export default function Feed({ events }) {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="relative">
-      {tempData.map((item) => (
+      {events.map((item) => (
         <div className="bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 m-3 p-2 flex flex-row justify-between">
           <div className="">
             <p>
@@ -97,8 +27,7 @@ export default function GlobalFeed() {
       {!open && (
         <button
           onClick={() => setOpen(true)}
-          className="fixed bottom-3 left-1/2 transform -translate-x-1/2 p-0 w-16 h-16 bg-yellow-400 rounded-full hover:bg-yellow-500 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
-        >
+          className="fixed bottom-3 left-1/2 transform -translate-x-1/2 p-0 w-16 h-16 bg-yellow-400 rounded-full hover:bg-yellow-500 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
           <PlusIcon className="w-8 h-8 mx-auto self-center" />
         </button>
       )}
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useNostrEvents, dateToUnix } from "nostr-react";
 import Feed from "../components/Feed";
 
-export default function Feed() {
+export default function FeedPage() {
+  const now = useRef(new Date()); // Make sure current time isn't re-rendered
+
   const { events } = useNostrEvents({
     filter: {
       since: dateToUnix(now.current), // all new events from now
@@ -10,9 +12,17 @@ export default function Feed() {
     },
   });
 
-  const filteredEvents = events.filter((event) => {
-    return event.content.includes("Loyalty hackathon Test Message 👐🏻👹");
-  });
+  const filteredEvents = events
+    .filter((event) => {
+      return event.content.includes("Loyalty hackathon Test Message 👐🏻👹");
+    })
+    .map((event) => {
+      return {
+        user: event.pubkey,
+        cash: event.content.split(" ")[event.content.split(" ").length - 1],
+        distance: "10km",
+      };
+    });
 
   return <Feed events={filteredEvents} />;
 }
