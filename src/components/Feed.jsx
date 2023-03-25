35b34fcbab9ffcc74c37e6c74a6f75777e1e30be
mtsarah <sarah_berger@outlook.com>
2023-03-25 01:51:41 +0100

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 import { useNostrEvents, dateToUnix } from "nostr-react";
+import { MapPinIcon, PlusIcon } from "@heroicons/react/24/outline";
+import Button from "./Button";
 
 export default function GlobalFeed() {
   const now = useRef(new Date()); // Make sure current time isn't re-rendered
@@ -52,20 +54,56 @@ export default function GlobalFeed() {
       cash: "40€",
       distance: "10km",
     },
+    {
+      user: "skleeeedjf",
+      cash: "40€",
+      distance: "10km",
+    },
+    {
+      user: "skldjf",
+      cash: "40€",
+      distance: "10km",
+    },
+    {
+      user: "skldjf",
+      cash: "40€",
+      distance: "10km",
+    },
+    {
+      user: "skldjf",
+      cash: "40€",
+      distance: "10km",
+    },
+    {
+      user: "skldjf",
+      cash: "40€",
+      distance: "10km",
+    },
   ];
 
   return (
     <div className="relative">
       {tempData.map((item) => (
-        <div className="bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 m-5">
-          <p>
-            {item.user} needs {item.cash}
-          </p>
+        <div className="bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 m-3 p-2 flex flex-row justify-between">
+          <div className="">
+            <p>
+              {item.user} needs {item.cash}
+            </p>
+            <p>
+              <MapPinIcon className="w-5 h-5 inline pb-1" /> {item.distance}{" "}
+              away
+            </p>
+          </div>
+          <div className="flex content-around p-2">
+            <Button>Help out</Button>
+          </div>
         </div>
       ))}
-      <button className="p-0 w-16 h-16 bg-yellow-400 rounded-full hover:bg-yellow-500 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
-        +
-      </button>
+      <div className="flex items-center">
+        <button className="fixed bottom-3 inset-x-auto p-0 w-16 h-16 bg-yellow-400 rounded-full hover:bg-yellow-500 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+          <PlusIcon className="w-8 h-8 mx-auto self-center" />
+        </button>
+      </div>
     </div>
   );
 }
